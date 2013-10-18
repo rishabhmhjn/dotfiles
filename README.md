a87365994473892de70d1bbefbb6ce91fc892111
Rishabh Mahajan <rishabhmhjn@gmail.com>
2013-10-18 15:17:45 +0900

    Adding initial files

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 dotfiles
 ========
+
+
+Installation
+------------
+
+### Prerequisites
+* Git
+
+
+
+### Bootstrapper
+
+
+``` bash-session
+$ bash < <( curl https://raw.github.com/rishabhmhjn/dotfiles/master/bootstrap.sh )
+```
+
+Don't worry, all your old files will be backed up!
+
+
+### Stay Updated
+
+Run the bootstrapper again!
+
+``` bash-session
+$ ~/.dotfiles/bootstrap.sh
+```
+
+
+Shell
+-----
+
+Most of the shell junk is setup to work in both zsh and bash. Bash users should
+see [.bash_profile](https://github.com/rishabhmhjn/dotfiles/blob/master/.bash_profile)
+and [.bash_prompt](https://github.com/rishabhmhjn/dotfiles/blob/master/.bash_prompt).
+
+
+### Aliases
+
+Check out [.aliases](https://github.com/rishabhmhjn/dotfiles/blob/master/.aliases)
+
+
+Git
+---
+
+I've included some handy git script additions as well as configution changes.
+Have a look at
+[.gitconfig](https://github.com/rishabhmhjn/dotfiles/blob/master/.gitconfig) to see
+various aliases and settings.
+
+Additional scripts (see [.scripts](https://github.com/rishabhmhjn/dotfiles/tree/master/.scripts/) directory for source):
---
+++ bootstrap.sh
@@ -0,0 +1,134 @@
+#!/bin/sh
+
+#-----------------------------------------------------------------------------
+# Functions
+#-----------------------------------------------------------------------------
+
+# Notice title
+notice() { echo  "\033[0;34m=> $1\033[0m"; }
+
+# Error title
+error() { echo  "\033[0;31m=> Error: $1\033[0m"; }
+
+# List item
+c_list() { echo  "  \033[0;34m✔\033[0m $1"; }
+
+# Error list item
+e_list() { echo  "  \033[0;31m✖\033[0m $1"; }
+
+# Check for dependency
+dep() {
+  type -p $1 &> /dev/null
+  local installed=$?
+  if [ $installed -eq 0 ]; then
+    c_list $1
+  else
+    e_list $1
+  fi
+  return $installed
+}
+
+backup() {
+  mkdir -p $backupdir
+
+  local files=( $(ls -a) )
+  for file in "${files[@]}"; do
+    in_array $file "${excluded[@]}" || cp -Rf "$HOME/$file" "$backupdir/$file"
+  done
+}
+
+install() {
+  local files=( $(ls -a) )
+  for file in "${files[@]}"; do
+    in_array $file "${excluded[@]}"
+    should_install=$?
+    if [ $should_install -gt 0 ]; then
+      [ -d "$HOME/$file" ] && rm -rf "$HOME/$file"
+      cp -Rf "$file" "$HOME/$file"
+    fi
+  done
+}
+
+in_array() {
+  local hay needle=$1
+  shift
+  for hay; do
+    [[ $hay == $needle ]] && return 0
+  done
+  return 1
+}
+
+
+#-----------------------------------------------------------------------------
+# Initialize
+#-----------------------------------------------------------------------------
+
+backupdir="$HOME/.dotfiles-backup/$(date "+%Y%m%d%H%M.%S")"
+dependencies=(git vim)
+excluded=(. .. .git .gitignore bootstrap.sh Gemfile Gemfile.lock Rakefile README.md .DS_Store)
+
+
+#-----------------------------------------------------------------------------
+# Dependencies
+#-----------------------------------------------------------------------------
+
+notice "Checking dependencies"
+
+not_met=0
+for need in "${dependencies[@]}"; do
+  dep $need
+  met=$?
+  not_met=$(echo "$not_met + $met" | bc)
+done
+
+if [ $not_met -gt "0" ]; then
+  error "$not_met dependencies not met!"
+  exit 1
+fi
+
+
+#-----------------------------------------------------------------------------
+# Install
+#-----------------------------------------------------------------------------
+
+# Assumes $HOME/.dotfiles is *ours*
+if [ -d $HOME/.dotfiles ]; then
+  pushd $HOME/.dotfiles
+
+  # Update Repo
+  notice "Updating"
+  git pull origin master
+  git submodule init
+  git submodule update
+
+  # Backup
+  notice "Backup up old files ($backupdir)"
+  backup
+
+  # Install
+  notice "Installing"
+  install
+else
+  # Clone Repo
+  notice "Downloading"
+  git clone --recursive git://github.com/rishabhmhjn/dotfiles.git $HOME/.dotfiles
+
+  pushd $HOME/.dotfiles
+
+  # Backup
+  notice "Backup up old files ($backupdir)"
+  backup
+
+  # Install
+  notice "Installing"
+  install
+fi
+
+
+#-----------------------------------------------------------------------------
+# Finished
+#-----------------------------------------------------------------------------
+
+popd
+notice "Done"
+# exec $SHELL -l
